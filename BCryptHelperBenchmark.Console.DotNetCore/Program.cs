26ade6e380d0a3856ec47af8b06a72e0d3408e5f
savissimo <simone.saviolo@gmail.com>
2018-11-23 12:04:44 +0100

    Added `tests` command line parameter

--- BCryptHelperBenchmark.Console.DotNetCore/Program.cs
+++ BCryptHelperBenchmark.Console.DotNetCore/Program.cs
@@ -7,23 +7,32 @@ namespace BCryptHelperBenchmark.Console.DotNetCore
 	{
 		static void Main(string[] args)
 		{
+			int numIterations = 100;
+
+			for (int i = 0; i < args.Length; ++i)
+			{
+				if (args[i] == "--tests")
+				{
+					int.TryParse(args[++i], out numIterations);
+				}
+			}
+
 			System.Console.WriteLine("Work factor\tTime taken (ms)");
 			for (int workFactor = 4; workFactor < 20; ++workFactor)
 			{
 				DateTime start = DateTime.Now;
 
-				int numIterations = 100;
 				for (int i = 0; i < numIterations; ++i)
 				{
-					BCryptHelper.HashPassword("prova", BCryptHelper.GenerateSalt(workFactor));
+					BCryptHelper.HashPassword("just a test", BCryptHelper.GenerateSalt(workFactor));
 					if (i % 10 == 0)
 					{
-						System.Console.Write("{0}\t\tMeasuring ({1}%)...\r", workFactor, (float)i / numIterations * 100);
+						System.Console.Write("{0}\t\tMeasuring ({1:0}%)...\r", workFactor, (float)i / numIterations * 100);
 					}
 				}
 
 				DateTime end = DateTime.Now;
-				System.Console.WriteLine("{0}\t\t{1} ms\t\t\t", workFactor, end.Subtract(start).TotalMilliseconds / numIterations);
+				System.Console.WriteLine("{0}\t\t{1:0.0} ms\t\t\t", workFactor, end.Subtract(start).TotalMilliseconds / numIterations);
 			}
 		}
 	}
